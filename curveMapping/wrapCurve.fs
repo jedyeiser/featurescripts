84a06b8d4c922a6b2573e86d1a1f3b1690e726d2
jedyeiser <jed.yeiser@gmail.com>
2026-02-10 21:38:51 -0800

    [AUTO-BACKUP] Before pushing xSection to Onshape
    Before push to Onshape at 2026-02-10 21:38:51

---
+++ curveMapping/wrapCurve.fs
@@ -0,0 +1,11 @@
+FeatureScript 2878;
+import(path : "onshape/std/common.fs", version : "2878.0");
+
+//import curveChain
+import(path : "670e82ad72abc97906ec9038", version : "a014a924eabd5caabe07c144");
+//import curveMappingUtils
+import(path : "de955d503dbb0ec88622e51b", version : "a677b5e4a3d7238db49bf848");
+//import curveMappingUtils
+import(path : "de955d503dbb0ec88622e51b", version : "a677b5e4a3d7238db49bf848");
+//import tools/bspline_data
+import(path : "b1e8bfe71f67389ca210ed8b/910a6d7a356c2832de31817a/b1c7f2116fb64e6b40bf53f4", version : "4fe0cca8e00a4cd812896a8c");
