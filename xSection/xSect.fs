aa5b907a92ebac4bbc77b23f1e4060cd3edab3cb
jedyeiser <jed.yeiser@gmail.com>
2026-02-20 20:10:33 -0800

    [AUTO-BACKUP] Before pushing xSection to Onshape
    Before push to Onshape at 2026-02-20 20:10:33

--- xSection/xSect.fs
+++ xSection/xSect.fs
@@ -218,7 +218,10 @@ export function elFunc(context is Context, id is Id, oldDefinition is map, defin
                         " [7]Q12="       ~ toString(r[7])  ~
                         " [8]Q66="       ~ toString(r[8])  ~
                         " [9]Q16="       ~ toString(r[9])  ~
-                        " [10]Q26="      ~ toString(r[10]));
+                        " [10]Q26="      ~ toString(r[10]) ~
+                        " [11]CTE_x="    ~ (size(r) > 11 ? toString(r[11]) : "n/a") ~
+                        " [12]CTE_y="    ~ (size(r) > 12 ? toString(r[12]) : "n/a") ~
+                        " [13]dims(len)=" ~ (size(r) > 13 ? toString(size(r[13])) : "n/a"));
             }
         }
 
