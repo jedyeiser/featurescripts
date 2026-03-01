983f31380270983a72648ff1727fe7c823ea543e
jedyeiser <jed.yeiser@gmail.com>
2026-02-28 22:43:42 -0800

    [AUTO-BACKUP] Before pushing curveMapping to Onshape
    Before push to Onshape at 2026-02-28 22:43:41

--- curveMapping/deform.fs
+++ curveMapping/deform.fs
@@ -513,11 +513,11 @@ export const deform = defineFeature(function(context is Context, id is Id, defin
             if (size(surfaceBodiesList) > 1)
             {
                 opBoolean(context, id + "unionFaces", {
-                    "tools"                       : surfaceBodies,
-                    "operationType"               : BooleanOperationType.UNION,
-                    "allowSheets"                 : true,
-                    "targetsAndToolsNeedGrouping" : true,
-                    "eraseImprintedEdges"         : true,
+                    "tools"              : surfaceBodies,
+                    "operationType"      : BooleanOperationType.UNION,
+                    "allowSheets"        : true,
+                    "makeSolid"          : false,
+                    "eraseImprintedEdges": true,
                 });
             }
         }
