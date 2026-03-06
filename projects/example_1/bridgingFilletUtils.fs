cb8180dcbb03519682e96a86399c4c62d27d0298
jedyeiser <jed.yeiser@gmail.com>
2026-03-06 02:34:52 -0800

    [AUTO-BACKUP] Before pulling xSection from Onshape
    Before pull from Onshape at 2026-03-06 02:34:52

---
+++ projects/example_1/bridgingFilletUtils.fs
@@ -0,0 +1,7 @@
+FeatureScript 2892;
+import(path : "onshape/std/common.fs", version : "2892.0");
+
+//import tools/bspline_data
+import(path : "b1e8bfe71f67389ca210ed8b/71a714bb442c2a2dabd1278a/b1c7f2116fb64e6b40bf53f4", version : "4fe0cca8e00a4cd812896a8c");
+//import tools/arc_length
+import(path : "b1e8bfe71f67389ca210ed8b/71a714bb442c2a2dabd1278a/f88f68e9ff3cb3c30d4afffe", version : "561709ffbf7a138328bbffc4");
